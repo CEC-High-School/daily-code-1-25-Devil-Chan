978e8a41af76326591e64d4aff5882646cc58374
Devil-Chan <35504477+Devil-Chan@users.noreply.github.com>
2018-01-25 12:54:08 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,16 @@
+#include <iostream>
+using namespace std;
+char main() {
+	char a;
+	char b;
+	char c;
+	cout << "your password ples" << endl;
+	cin >> a;
+	cin >> b; 
+	cin >> c;
 
-
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	if (a == 'N' && b == 'E'&& c == 'S')
+		cout << "correct!" << endl;
+	else
+		cout << "you suck!" << endl;
+}
